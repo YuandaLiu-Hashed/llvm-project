a44f73cd20e736ca71223829265f4b2fa116e596
Yuanda Liu <72162739+YuandaLiu-Hashed@users.noreply.github.com>
2022-12-18 01:03:01 -0500

    Add support for any combination of jump conditions

--- llvm/lib/Target/Scott8/MCTargetDesc/Scott8InstPrinter.cpp
+++ llvm/lib/Target/Scott8/MCTargetDesc/Scott8InstPrinter.cpp
@@ -60,24 +60,28 @@ void Scott8InstPrinter::printOperand(const MCInst *MI, unsigned OpNo, const MCSu
 
 void Scott8InstPrinter::printCCOperand(const MCInst *MI, unsigned OpNo, const MCSubtargetInfo &STI, raw_ostream &O) {
   unsigned CC = MI->getOperand(OpNo).getImm();
+  if (CC & Scott8CC::COND_C) O << "C";
+  if (CC & Scott8CC::COND_A) O << "A";
+  if (CC & Scott8CC::COND_E) O << "E";
+  if (CC & Scott8CC::COND_Z) O << "Z";
 
-  switch (CC) {
-    default:
-      llvm_unreachable("Unsupported CC code");
-    case Scott8CC::COND_A:
-      O<< "A";
-      break;
-    case Scott8CC::COND_C:
-      O<< "C";
-      break;
-    case Scott8CC::COND_E:
-      O<< "E";
-      break;
-    case Scott8CC::COND_Z:
-      O<< "Z";
-      break;
-    case Scott8CC::COND_AE:
-      O<< "AE";
-      break;
-  }
+  // switch (CC) {
+  //   default:
+  //     llvm_unreachable("Unsupported CC code");
+  //   case Scott8CC::COND_A:
+  //     O<< "A";
+  //     break;
+  //   case Scott8CC::COND_C:
+  //     O<< "C";
+  //     break;
+  //   case Scott8CC::COND_E:
+  //     O<< "E";
+  //     break;
+  //   case Scott8CC::COND_Z:
+  //     O<< "Z";
+  //     break;
+  //   case Scott8CC::COND_AE:
+  //     O<< "AE";
+  //     break;
+  // }
 }
